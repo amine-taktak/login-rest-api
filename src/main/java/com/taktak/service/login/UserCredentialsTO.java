30c4838157482dbbddf494f549172aeac9877997
amine-taktak <amine.taktak@hotmail.com>
2024-03-15 22:44:46 +0100

    added a UserCredentialsTO for the authentication process

---
+++ src/main/java/com/taktak/service/login/UserCredentialsTO.java
@@ -0,0 +1,28 @@
+package com.taktak.service.login;
+
+/*
+ * @Author Amine Taktak
+ * @project login-rest-api
+ * @created 15.03.21:37
+ */
+public class UserCredentialsTO {
+
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
