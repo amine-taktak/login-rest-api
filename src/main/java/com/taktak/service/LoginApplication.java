d2563c19b44d22f31b84beb3a5c4102fa05230fb
amine-taktak <amine.taktak@hotmail.com>
2024-05-09 13:15:22 +0200

    Revert "solved servlet mapping problem."
    This reverts commit c3a98f6e64bebfe04628ba587a4d9d78be8587c0.

---
+++ src/main/java/com/taktak/service/LoginApplication.java
@@ -0,0 +1,11 @@
+package com.taktak.service;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/rest")
+public class LoginApplication extends Application {
+
+}
+
+
